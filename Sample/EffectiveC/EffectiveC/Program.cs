620de9272fe37472080059b5ccb008de0f513688
ccg1120 <ccg1120@naver.com>
2023-04-16 18:27:14 +0900

    테스트 프로젝트 업로드

---
+++ Sample/EffectiveC/EffectiveC/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveC
+{
+
+    //class MyType
+    //{
+    //    public void DoStuff()
+    //    {
+
+    //    }
+
+    //}
+
+
+    //class Program
+    //{
+    //    public void test(IEnumerable theCollection)
+    //    {
+    //        foreach (MyType t in theCollection)
+    //            t.DoStuff();
+
+    //        IEnumerator it = theCollection.GetEnumerator();
+    //        while (it.MoveNext())
+    //        {
+    //            MyType t = (MyType)it.Current;
+    //            t.DoStuff();
+    //        }
+
+    //        IEnumerable collection = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+    //        var collection2 = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+    //    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //int intValue = 10;
+
+            //object o = intValue;
+
+            //var intTest = o as int?;
+            int test = 1 + EffectiveC_Library.Class1.ConstInt;
+            int test2 = 2 + EffectiveC_Library.Class1.StaticReadOnlyInt;
+            Console.WriteLine($"ConstInt = {EffectiveC_Library.Class1.ConstInt}");
+            Console.WriteLine($"StaticReadOnlyInt = {EffectiveC_Library.Class1.StaticReadOnlyInt}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sample/EffectiveC/EffectiveC_Library/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveC_Library
+{
+    public class Class1
+    {
+        public const int ConstInt = 10000;
+        public static readonly int StaticReadOnlyInt = 9;
+
+    }
+}
